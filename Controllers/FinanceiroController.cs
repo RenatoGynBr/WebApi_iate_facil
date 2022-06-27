509c8c07cbbf19f464f4b60eab7d52c5d8fe63cb
Renato Cavalcante <ny.renato@gmail.com>
2022-06-27 20:59:51 -0300

    FinanceiroController.cs

--- Controllers/FinanceiroController.cs
+++ Controllers/FinanceiroController.cs
@@ -16,14 +16,165 @@ namespace WebApi_iate_facil.Controllers
         {
             _config = config;
         }
-        /*
-        SP_APP_PARCELA_CARNE
-        SP_APP_LISTA_CARNE
-        SP_APP_LANC_FUTURO
-        SP_APP_DET_CARNE
-        SP_APP_DECLARACAO_QUITACAO
 
-         */
+        [HttpGet]
+        public JsonResult StoredProcParcelaCarne(int seqParcela)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_PARCELA_CARNE 1";
+                string query = $"EXEC SP_APP_PARCELA_CARNE {seqParcela}";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcParcelaCarne error: " + e.Message);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult StoredProcListaCarne(string usuario)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_LISTA_CARNE '01000100'";
+                string query = $"EXEC SP_APP_LISTA_CARNE '{usuario}'";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcListaCarne error: " + e.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public JsonResult StoredProcLancFuturo(string usuario)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_LANC_FUTURO '01000100'";
+                string query = $"EXEC SP_APP_LANC_FUTURO '{usuario}'";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcLancFuturo error: " + e.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult StoredProcDetCarne(int seqParcela)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_DET_CARNE 1";
+                string query = $"EXEC SP_APP_DET_CARNE {seqParcela}";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcDetCarne error: " + e.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult StoredProcDeclaracaoQuitacao(int aaReferencia, string usuario, string ip)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_DECLARACAO_QUITACAO 1234,'01000100','abcdefghij'";
+                string query = $"EXEC SP_APP_DECLARACAO_QUITACAO {aaReferencia},'{usuario}','{ip}'";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcDeclaracaoQuitacao error: " + e.Message);
+            }
+        }
+
 
     }
 }
