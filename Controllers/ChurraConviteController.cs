5411a96c4f48e68e48571fe70477361748d49127
Renato Cavalcante <ny.renato@gmail.com>
2022-06-30 22:26:39 -0300

    ChurraConviteController.cs

--- Controllers/ChurraConviteController.cs
+++ Controllers/ChurraConviteController.cs
@@ -18,37 +18,132 @@ namespace WebApi_iate_facil.Controllers
             _config = config;
         }
 
-        /*
-SP_APP_SALDO_CONV_CHURRASQUEIRA
-Todos os parâmetros são obrigatórios.
+        [HttpGet]
+        public JsonResult StoredProcSaldoConviteChurrasqueira(int seqReserva)
+        {
+            try
+            {
+                //string query = @"exec SP_APP_SALDO_CONV_CHURRASQUEIRA 123";
+                string query = $"EXEC SP_APP_SALDO_CONV_CHURRASQUEIRA {seqReserva}";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
 
-SP_APP_INCLUI_CONV_CHURRASQUEIRA
-	Parâmetros obrigatórios:
-		@USER_ACESSO_SISTEMA		
-		@NOME_CONVIDADO
-		@CPF_CONVIDADO
-		@DT_NASC_CONVIDADO
-		@NR_DOC_ESTRANGEIRO
-		@CD_CATEGORIA_CONVITE
-		@IP	
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
 
-Neste caso existe uma regra para preenchimento dos campos:
-@CPF_CONVIDADO		
-@DT_NASC_CONVIDADO		
-@NR_DOC_ESTRANGEIRO	
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcSaldoConviteChurrasqueira error: " + e.Message);
+            }
+        }
 
-Se o @CPF_CONVIDADO for informado os outros 2 campos são opcionais
-Se o @NR_DOC_ESTRANGEIRO for informado os outros 2 campos são opcionais
-Se a @DT_NASC_CONVIDADO for informada e for menor de idade os outros 2 campos são opcionais, caso contrário é obrigatório informar um dos outros 2 campos
-Se a @DT_NASC_CONVIDADO é obrigatório informar um dos outros 2 campos
-		
-SP_APP_REC_CONV_CHURRASQUEIRA
-Todos os parâmetros são obrigatórios.
+        [HttpGet]
+        public JsonResult StoredProcRecConviteChurrasqueira(int seqReserva)
+        {
+            try
+            {
+                //string query = @"exec SP_APP_REC_CONV_CHURRASQUEIRA 123";
+                string query = $"EXEC SP_APP_REC_CONV_CHURRASQUEIRA {seqReserva}";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
 
-SP_APP_CANCELA_CONV_CHURRASQUEIRA
-Todos os parâmetros são obrigatórios.
-          
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
 
-         */
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcRecConviteChurrasqueira error: " + e.Message);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult StoredProcIncluiConviteChurrasqueira(string usuario, string nomeConvidado, string cpfConvidado,
+            string dataNascimento, string nrDocEstrangeiro, string categoriaConvite, string ip)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_INCLUI_CONV_CHURRASQUEIRA '01000100','abcdefghij'";
+                string query = $"EXEC SP_APP_INCLUI_CONV_CHURRASQUEIRA '{usuario}','{nomeConvidado}','{cpfConvidado}'," +
+                    $"'{dataNascimento}', '{nrDocEstrangeiro}', '{categoriaConvite}', '{ip}'";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcIncluiConviteChurrasqueira error: " + e.Message);
+            }
+        }
+
+        [HttpPut]
+        public JsonResult StoredProcCancelaConviteChurrasqueira(int numeroConvite, string usuario, string ip)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_CANCELA_CONV_CHURRASQUEIRA 1234,'01000100','abcdefghij'";
+                string query = $"EXEC SP_APP_CANCELA_CONV_CHURRASQUEIRA {numeroConvite},'{usuario}','{ip}'";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcCancelaConviteChurrasqueira error: " + e.Message);
+            }
+        }
+
+
     }
 }
--- Controllers/ChurraReservaController.cs
+++ Controllers/ChurraReservaController.cs
@@ -50,37 +50,6 @@ namespace WebApi_iate_facil.Controllers
         }
 
 
-        [HttpPost]
-        public JsonResult StoredProcReservaChurrasqueira(int seqDependencia, string usuario, string dataReserva, string periodo)
-        {
-            try
-            {
-                //string query = @"EXEC SP_APP_RESERVA_CHURRASQUEIRA 1,'01000100','2022-07-31','P1'";
-                string query = $"EXEC SP_APP_RESERVA_CHURRASQUEIRA {seqDependencia}, '{usuario}', '{dataReserva}', '{periodo}'";
-                DataTable table = new DataTable();
-                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
-
-                SqlDataReader myReader;
-                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
-                {
-                    myConn.Open();
-                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
-                    {
-                        myReader = myCommand.ExecuteReader();
-                        table.Load(myReader);
-                        myReader.Close();
-                        myConn.Close();
-                    }
-                }
-
-                return new JsonResult(table);
-            }
-            catch (System.Exception e)
-            {
-                throw new Exception("HttpGet StoredProcConsultaChurrasqueira error: " + e.Message);
-            }
-        }
-
         [HttpGet]
         public JsonResult StoredProcRecReservaChurrasqueira(string usuario)
         {
@@ -109,6 +78,37 @@ namespace WebApi_iate_facil.Controllers
             catch (System.Exception e)
             {
                 throw new Exception("HttpGet StoredProcRecReservaChurrasqueira error: " + e.Message);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult StoredProcReservaChurrasqueira(int seqDependencia, string usuario, string dataReserva, string periodo)
+        {
+            try
+            {
+                //string query = @"EXEC SP_APP_RESERVA_CHURRASQUEIRA 1,'01000100','2022-07-31','P1'";
+                string query = $"EXEC SP_APP_RESERVA_CHURRASQUEIRA {seqDependencia}, '{usuario}', '{dataReserva}', '{periodo}'";
+                DataTable table = new DataTable();
+                string sqlDataSource = _config.GetConnectionString("DefaultConnection");
+
+                SqlDataReader myReader;
+                using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+                {
+                    myConn.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myConn.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception("HttpGet StoredProcConsultaChurrasqueira error: " + e.Message);
             }
         }
 
