63ded991bc801098fb2a9cf9c30d807a2e482b22
Renato Cavalcante <ny.renato@gmail.com>
2022-07-03 15:11:32 -0300

    Campos opcionais no método StoredProcIncluiConviteChurrasqueira;

--- Controllers/ChurraConviteController.cs
+++ Controllers/ChurraConviteController.cs
@@ -80,15 +80,37 @@ namespace WebApi_iate_facil.Controllers
             }
         }
 
+        /*
+         SP_APP_INCLUI_CONV_CHURRASQUEIRA
+        	Parâmetros obrigatórios:
+    		@USER_ACESSO_SISTEMA		
+	    	@NOME_CONVIDADO
+		    @CPF_CONVIDADO
+    		@DT_NASC_CONVIDADO
+	    	@NR_DOC_ESTRANGEIRO
+		    @CD_CATEGORIA_CONVITE
+    		@IP	
+
+        Neste caso existe uma regra para preenchimento dos campos:
+            @CPF_CONVIDADO		
+            @DT_NASC_CONVIDADO		
+            @NR_DOC_ESTRANGEIRO	
+
+        Se o @CPF_CONVIDADO for informado os outros 2 campos são opcionais
+        Se o @NR_DOC_ESTRANGEIRO for informado os outros 2 campos são opcionais
+        Se a @DT_NASC_CONVIDADO for informada e for menor de idade os outros 2 campos são opcionais, caso contrário é obrigatório informar um dos outros 2 campos
+        Se a @DT_NASC_CONVIDADO é obrigatório informar um dos outros 2 campos
+         */
         [HttpPost]
-        public JsonResult StoredProcIncluiConviteChurrasqueira(string usuario, string nomeConvidado, string cpfConvidado,
-            string dataNascimento, string docEstrangeiro, string categoriaConvite, string ip)
+        public JsonResult StoredProcIncluiConviteChurrasqueira(int reserva, string usuario, string nomeConvidado, string cpfConvidado,
+            string dataNascimento, string docEstrangeiro, string ip)
         {
             try
             {
                 //string query = @"EXEC SP_APP_INCLUI_CONV_CHURRASQUEIRA '01000100','abcdefghij'";
-                string query = $"EXEC SP_APP_INCLUI_CONV_CHURRASQUEIRA '{usuario}','{nomeConvidado}','{cpfConvidado}'," +
-                    $"'{dataNascimento}', '{docEstrangeiro}', '{categoriaConvite}', '{ip}'";
+                string query = ($"EXEC SP_APP_INCLUI_CONV_CHURRASQUEIRA {reserva},'{usuario}','{nomeConvidado}','{cpfConvidado}'," +
+                    $"'{dataNascimento}', '{docEstrangeiro}', '{ip}'")
+                    .Replace("''", "null").Replace(",,", ",null,"); // Replace strings e numéricos
                 DataTable table = new DataTable();
                 string sqlDataSource = _config.GetConnectionString("DefaultConnection");
 
